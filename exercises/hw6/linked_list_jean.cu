e7a1793c53868175b99a2822e44618d89386ed6d
lebermuda <jean35lb@gmail.com>
2022-10-13 13:32:02 +0200

    HW6

--- exercises/hw6/linked_list_jean.cu
+++ exercises/hw6/linked_list_jean.cu
@@ -11,6 +11,26 @@ struct list_elem {
     list_elem *next;
 };
 
+template <typename T>
+void alloc_bytes(T &ptr, size_t num_bytes){
+
+    ptr = (T)malloc(num_bytes);
+}
+
+__host__ __device__
+void print_element(list_elem *list, int ele_num) {
+    list_elem *elem = list;
+    for (int i = 0; i < ele_num; i++) {
+        elem = elem->next;
+        printf("key = %d\n", elem->key);
+    }
+}
+
+__global__ void gpu_print_element(list_elem *list, int ele_num){
+    print_element(list, ele_num);
+}
+const int ele = 3;
+
 
 
 __global__ void gpu_kernel(double* matrix,int n){
@@ -44,23 +64,35 @@ void addElement(double* matrix, int n, int i_start,int j_start){
 const int num_elem = 5;
 int main(){
 
-    double* matrix ;
+//    double* matrix ;
+//
+//    cudaMallocManaged(&matrix,num_elem*num_elem*sizeof(double));
+//
+//    dim3 dimGrid (1,1,1);
+//    dim3 dimBlock (num_elem-2,num_elem,1);
+//    gpu_kernel<<<dimGrid,dimBlock>>>(matrix,num_elem);
+//
+//    //MARCHE PAS EN PARALLEL
+//    //addElement(matrix,num_elem,num_elem-2,0);
+//
+//    cudaDeviceSynchronize();
+//
+//    //MARCHE EN SEQUENTIEL
+//    //addElement(matrix,num_elem,num_elem-2,0);
+//
+//
+//    printMatrix(matrix,num_elem);
+//
+//    cudaFree(matrix);
 
-    cudaMallocManaged(&matrix,num_elem*num_elem*sizeof(double));
-
-    dim3 dimGrid (1,1,1);
-    dim3 dimBlock (num_elem-2,num_elem,1);
-    gpu_kernel<<<dimGrid,dimBlock>>>(matrix,num_elem);
-
-    addElement(matrix,num_elem,num_elem-2,0);
-
+    list_elem *list_base, *list;
+    alloc_bytes(list_base, sizeof(list_elem));
+    list = list_base;
+    for (int i = 0; i < num_elem; i++){
+        list->key = i;
+        alloc_bytes(list->next, sizeof(list_elem));
+        list = list->next;}
+    print_element(list_base, ele);
+    //gpu_print_element<<<1,1>>>(list_base, ele);
     cudaDeviceSynchronize();
-
-    //addElement(matrix,num_elem,num_elem-2,0);
-
-
-    printMatrix(matrix,num_elem);
-
-    cudaFree(matrix);
-
 }
