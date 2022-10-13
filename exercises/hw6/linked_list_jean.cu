6c3482bc155dc3e53899e38b5629d58ed4ae6ccb
lebermuda <jean35lb@gmail.com>
2022-10-13 12:47:51 +0200

    HW6

--- exercises/hw6/linked_list_jean.cu
+++ exercises/hw6/linked_list_jean.cu
@@ -53,8 +53,13 @@ int main(){
         list->key = i;
         alloc_bytes(list->next, sizeof(list_elem));
         list = list->next;}
-    print_element(list_base, ele);
+
+    //print_element(list_base, ele);
+
     gpu_print_element<<<1,1>>>(list_base, ele);
+
     cudaDeviceSynchronize();
+
+
     //cudaCheckErrors("cuda error!");
 }
