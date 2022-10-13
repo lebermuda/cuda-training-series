79c00f1c7990de577f4d5984692f72b3e44ad2ca
lebermuda <jean35lb@gmail.com>
2022-10-13 13:40:17 +0200

    HW6

--- exercises/hw6/array_inc_jean.cu
+++ exercises/hw6/array_inc_jean.cu
@@ -22,20 +22,32 @@ __global__ void inc(int *array, size_t n){
 const size_t  ds = 32ULL*1024ULL*1024ULL;
 
 int main(){
+    int *h_array;
+    cudaMallocManaged(&h_array, ds*sizeof(d_array[0]));
 
-  int *h_array, *d_array;
-  alloc_bytes(h_array, ds*sizeof(h_array[0]));
-  cudaMalloc(&d_array, ds*sizeof(d_array[0]));
-  //cudaCheckErrors("cudaMalloc Error");
-  memset(h_array, 0, ds*sizeof(h_array[0]));
-  cudaMemcpy(d_array, h_array, ds*sizeof(h_array[0]), cudaMemcpyHostToDevice);
-  //cudaCheckErrors("cudaMemcpy H->D Error");
-  inc<<<256, 256>>>(d_array, ds);
-  //cudaCheckErrors("kernel launch error");
-  cudaMemcpy(h_array, d_array, ds*sizeof(h_array[0]), cudaMemcpyDeviceToHost);
-  //cudaCheckErrors("kernel execution or cudaMemcpy D->H Error");
-  for (int i = 0; i < ds; i++) 
-    if (h_array[i] != 1) {printf("mismatch at %d, was: %d, expected: %d\n", i, h_array[i], 1); return -1;}
-  printf("success!\n"); 
-  return 0;
+    memset(h_array, 0, ds*sizeof(h_array[0]));
+
+    inc<<<256, 256>>>(d_array, ds);
+    cudaDeviceSynchronize();
+
+    for (int i = 0; i < ds; i++)
+        if (h_array[i] != 1) {printf("mismatch at %d, was: %d, expected: %d\n", i, h_array[i], 1); return -1;}
+    printf("success!\n");
+    return 0;
+
+//  int *h_array, *d_array;
+//  alloc_bytes(h_array, ds*sizeof(h_array[0]));
+//  cudaMalloc(&d_array, ds*sizeof(d_array[0]));
+//  //cudaCheckErrors("cudaMalloc Error");
+//  memset(h_array, 0, ds*sizeof(h_array[0]));
+//  cudaMemcpy(d_array, h_array, ds*sizeof(h_array[0]), cudaMemcpyHostToDevice);
+//  //cudaCheckErrors("cudaMemcpy H->D Error");
+//  inc<<<256, 256>>>(d_array, ds);
+//  //cudaCheckErrors("kernel launch error");
+//  cudaMemcpy(h_array, d_array, ds*sizeof(h_array[0]), cudaMemcpyDeviceToHost);
+//  //cudaCheckErrors("kernel execution or cudaMemcpy D->H Error");
+//  for (int i = 0; i < ds; i++)
+//    if (h_array[i] != 1) {printf("mismatch at %d, was: %d, expected: %d\n", i, h_array[i], 1); return -1;}
+//  printf("success!\n");
+//  return 0;
 }
